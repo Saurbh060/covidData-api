b92e72fcbb87f17c4d2ffe57213d62f16825c8d0
Saurbh060 <b17060@students.iitmandi.ac.in>
2022-03-22 07:25:20 +0530

    adding user's GPS input functionality

--- go.mod
+++ go.mod
@@ -3,11 +3,18 @@ module covidData-api
 go 1.17
 
 require (
+	github.com/erikstmartin/go-testdb v0.0.0-20160219214506-8d10e4a1bae5 // indirect
 	github.com/go-stack/stack v1.8.0 // indirect
 	github.com/golang/snappy v0.0.1 // indirect
+	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/jasonwinn/geocoder v0.0.0-20190118154513-0a8a678400b8 // indirect
+	github.com/kellydunn/golang-geo v0.7.0 // indirect
+	github.com/kelvins/geocoder v0.0.0-20200113010004-f579500e9e27 // indirect
 	github.com/klauspost/compress v1.13.6 // indirect
+	github.com/kylelemons/go-gypsy v1.0.0 // indirect
 	github.com/labstack/echo/v4 v4.7.1 // indirect
 	github.com/labstack/gommon v0.3.1 // indirect
+	github.com/lib/pq v1.10.4 // indirect
 	github.com/mattn/go-colorable v0.1.11 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
@@ -17,6 +24,8 @@ require (
 	github.com/xdg-go/scram v1.0.2 // indirect
 	github.com/xdg-go/stringprep v1.0.2 // indirect
 	github.com/youmark/pkcs8 v0.0.0-20181117223130-1be2e3e5546d // indirect
+	github.com/ziutek/mymysql v1.5.4 // indirect
+	github.com/zsefvlol/timezonemapper v1.0.0 // indirect
 	go.mongodb.org/mongo-driver v1.8.4 // indirect
 	golang.org/x/crypto v0.0.0-20210817164053-32db794688a5 // indirect
 	golang.org/x/net v0.0.0-20211015210444-4f30a5c0130f // indirect
--- server.go
+++ server.go
@@ -1,34 +1,42 @@
 package main
 
 import (
-	"net/http"
-	"fmt"
 	"encoding/json"
-    "io/ioutil"
-    "os"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
 	"time"
+
 	"github.com/labstack/echo/v4"
-	// "go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson"
+
+	"github.com/jasonwinn/geocoder"
 
 	"context"
-    "log"
+	"log"
 
-    "go.mongodb.org/mongo-driver/mongo"
-    "go.mongodb.org/mongo-driver/mongo/options"
 	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 type MongoFields struct {
-	State string `bson:"state"`
+	State      string  `bson:"state"`
 	TotalCases float64 `bson:"totalCases"`
 }
 
 type StateData struct {
-	ID     primitive.ObjectID `bson:"_id,omitempty"`
-	StateCases []MongoFields `bson:"stateCases" json:"stateCases"`
+	ID         primitive.ObjectID `bson:"_id,omitempty"`
+	StateCases []MongoFields      `bson:"stateCases" json:"stateCases"`
 }
 
-func Connect( mongoFiled StateData) {
+type Location struct {
+	Lat  float64 `json:"lat"`
+	Long float64 `json:"long"`
+}
+
+func Connect(mongoFiled StateData) {
 	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
 	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
 	if err != nil {
@@ -39,25 +47,11 @@ func Connect( mongoFiled StateData) {
 
 	database := client.Database("covidCases")
 	stateCollection := database.Collection("stateData")
-	
 
 	fmt.Println("printing mongo data")
 	fmt.Println(mongoFiled)
 
-	// data := StateData{
-	// 	StateCases: []MongoFields{
-	// 		{
-	// 			State:"AN",
-	// 			TotalCases : 23124,
-	// 		},
-	// 		{
-	// 			State:"AN",
-	// 			TotalCases : 23124,
-	// 		},
-	// 	},
-	// }
-	// fmt.Println("printing data", data)
-	insertManyResult, err := stateCollection.InsertOne(ctx,mongoFiled)
+	insertManyResult, err := stateCollection.InsertOne(ctx, mongoFiled)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -66,50 +60,157 @@ func Connect( mongoFiled StateData) {
 	fmt.Println("Connection to MongoDB closed.")
 }
 
+func ConnectAndGet(stateName string) MongoFields {
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Connected to MongoDB!")
+	defer client.Disconnect(ctx)
+
+	database := client.Database("covidCases")
+	stateCollection := database.Collection("stateData")
+
+	var podcast StateData
+	if err = stateCollection.FindOne(ctx, bson.M{}).Decode(&podcast); err != nil {
+		log.Fatal(err)
+	}
+
+	var result MongoFields
+	for i := 0; i < len(podcast.StateCases); i++ {
+		// fmt.Println("key", podcast.StateCases[i])
+		data := podcast.StateCases[i]
+		// fmt.Println("state:", data.State)
+		if data.State == stateName {
+			result.State = data.State
+			result.TotalCases = data.TotalCases
+			break
+		}
+	}
+	fmt.Println("Connection to MongoDB closed.")
+	return result
+}
+
 func saveCovidData(c echo.Context) error {
 
 	// Open our jsonFile
-    jsonFile, err := os.Open("data.min.json")
-    // if we os.Open returns an error then handle it
-    if err != nil {
-        fmt.Println(err)
-    }
-    fmt.Print("\n\nSuccessfully Opened users.json\n\n")
-    // defer the closing of our jsonFile so that we can parse it later on
-    defer jsonFile.Close()
+	jsonFile, err := os.Open("data.min.json")
+	// if we os.Open returns an error then handle it
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Print("\n\nSuccessfully Opened users.json\n\n")
+	// defer the closing of our jsonFile so that we can parse it later on
+	defer jsonFile.Close()
 
-    byteValue, _ := ioutil.ReadAll(jsonFile)
+	byteValue, _ := ioutil.ReadAll(jsonFile)
 
-    var result map[string]interface{}
-    json.Unmarshal([]byte(byteValue), &result)
+	var result map[string]interface{}
+	json.Unmarshal([]byte(byteValue), &result)
 
-	docs:= []MongoFields{} 
-	doc:= StateData{} 
-	
-	for key := range result {		
+	docs := []MongoFields{}
+	doc := StateData{}
+
+	for key := range result {
 		n := MongoFields{State: key, TotalCases: result[key].(map[string]interface{})["total"].(map[string]interface{})["confirmed"].(float64)}
-        docs = append(docs,n)
+		docs = append(docs, n)
 	}
 	doc.StateCases = docs
 	fmt.Println(docs)
 
-	Connect(doc )
+	Connect(doc)
 	return c.JSON(http.StatusOK, "success")
 }
 
 func getStateName(c echo.Context) error {
-	// User ID from path `users/:id`
-	state := c.Param("state")
-  return c.String(http.StatusOK, state)
+
+	var location Location
+
+	defer c.Request().Body.Close()
+
+	b, err := ioutil.ReadAll(c.Request().Body)
+
+	if err != nil {
+		log.Printf("Failed reading the request body %s", err)
+		return c.String(http.StatusInternalServerError, "")
+	}
+	err = json.Unmarshal(b, &location)
+	if err != nil {
+		log.Printf("Failed unmarshal  %s", err)
+		return c.String(http.StatusInternalServerError, "")
+	}
+
+	fmt.Println(location)
+	geocoder.SetAPIKey("Fmjtd%7Cluub256alu%2C7s%3Do5-9u82ur")
+	address, err := geocoder.ReverseGeocode(location.Lat, location.Long)
+	if err != nil {
+		panic("THERE WAS SOME ERROR!!!!!")
+	}
+
+	fmt.Println(address.State)
+
+	stateMapping := map[string]string{
+		"Andaman and Nicobar Islands": "AN",
+		"Andhra Pradesh":              "AP",
+		"Arunachal Pradesh":           "AR",
+		"Assam":                       "AS",
+		"Bihar":                       "BR",
+		"Chandigarh":                  "CH",
+		"Chhattisgarh":                "CT",
+		"Dadra and Nagar Haveli":      "DN",
+		"Daman and Diu":               "DD",
+		"Delhi":                       "DL",
+		"Goa":                         "GA",
+		"Gujarat":                     "GJ",
+		"Haryana":                     "HR",
+		"Himachal Pradesh":            "HP",
+		"Jammu and Kashmir":           "JK",
+		"Jharkhand":                   "JH",
+		"Karnataka":                   "KA",
+		"Kerala":                      "KL",
+		"Lakshadweep":                 "LD",
+		"Madhya Pradesh":              "MP",
+		"Maharashtra":                 "MH",
+		"Manipur":                     "MN",
+		"Meghalaya":                   "ML",
+		"Mizoram":                     "MZ",
+		"Nagaland":                    "NL",
+		"Odisha":                      "OR",
+		"Orissa":                      "OD",
+		"cherry":                      "PY",
+		"Punjab":                      "PB",
+		"Rajasthan":                   "RJ",
+		"Sikkim":                      "SK",
+		"Tamil Nadu":                  "TN",
+		"Telangana":                   "TG",
+		"Tripura":                     "TR",
+		"Uttar Pradesh":               "UP",
+		"Uttarakhand":                 "UT",
+		"West Bengal":                 "WB",
+	}
+
+	if v, found := stateMapping[address.State]; found {
+
+		fmt.Println("state code: ", v)
+		fetchedData := ConnectAndGet(v)
+
+		responseData := &MongoFields{
+			State:      fetchedData.State,
+			TotalCases: fetchedData.TotalCases,
+		}
+		fmt.Println("response: ", *responseData)
+		return c.JSON(http.StatusOK, responseData)
+	} else {
+		fmt.Println("Coordinates are not belongs to India")
+	}
+
+	return c.JSON(http.StatusOK, "Coordinates are not belongs to India")
 }
-
-var FILE_PATH = "./output-stats.json"
-
 
 func main() {
 	e := echo.New()
-	e.GET("/cases/:state", getStateName)
+	e.POST("/stateCases", getStateName)
 	e.GET("/saveCases", saveCovidData)
 	e.Logger.Fatal(e.Start(":1323"))
 }
-
